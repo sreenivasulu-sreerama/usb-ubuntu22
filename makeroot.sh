bd10286d92869a1ea4df350c7b55e50936cac0ab
Sreenivasulu Sreerama <sreenivasulu.sreerama@nokia.com>
2024-01-23 16:24:37 -0800

    Refined the script

--- makeroot.sh
+++ makeroot.sh
@@ -14,26 +14,31 @@ EOF
 apt clean all
 apt update
 
+# Set locale
+apt install --assume-yes --allow-downgrades language-pack-en/jammy
+localectl set-locale LANG=en_US.UTF-8
+
 # Add packages
-apt install --assume-yes accountsservice/jammy
-apt install --assume-yes build-essential/jammy
-apt install --assume-yes dnsutils/jammy
-apt install --assume-yes ssh/jammy
-apt install --assume-yes telnet/jammy
-apt install --assume-yes setserial/jammy
-apt install --assume-yes lsof/jammy
-apt install --assume-yes usbutils/jammy
-apt install --assume-yes ethtool/jammy
-apt install --assume-yes iptables/jammy
-apt install --assume-yes ebtables/jammy
-apt install --assume-yes gdisk/jammy
-apt install --assume-yes dmidecode/jammy
-apt install --assume-yes tcpdump/jammy
-apt install --assume-yes python/jammy python-pycurl/jammy python-serial/jammy
-apt install --assume-yes info/jammy lshw/jammy ntp/jammy
-apt install --assume-yes zip/jammy unzip/jammy
-apt install --assume-yes software-properties-common/jammy
-apt install --assume-yes libqmi-utils/jammy modemmanager/jammy
+apt install --assume-yes --allow-downgrades accountsservice/jammy
+apt install --assume-yes --allow-downgrades build-essential/jammy
+apt install --assume-yes --allow-downgrades dnsutils/jammy
+apt install --assume-yes --allow-downgrades ssh/jammy
+apt install --assume-yes --allow-downgrades telnet/jammy
+apt install --assume-yes --allow-downgrades setserial/jammy
+apt install --assume-yes --allow-downgrades lsof/jammy
+apt install --assume-yes --allow-downgrades usbutils/jammy
+apt install --assume-yes --allow-downgrades ethtool/jammy
+apt install --assume-yes --allow-downgrades iptables/jammy
+apt install --assume-yes --allow-downgrades ebtables/jammy
+apt install --assume-yes --allow-downgrades gdisk/jammy
+apt install --assume-yes --allow-downgrades dmidecode/jammy
+apt install --assume-yes --allow-downgrades tcpdump/jammy
+apt install --assume-yes --allow-downgrades python/jammy python-pycurl/jammy python-serial/jammy
+apt install --assume-yes --allow-downgrades info/jammy lshw/jammy ntp/jammy
+apt install --assume-yes --allow-downgrades zip/jammy unzip/jammy
+apt install --assume-yes --allow-downgrades software-properties-common/jammy
+apt install --assume-yes --allow-downgrades vim/jammy
+apt install --assume-yes --allow-downgrades netplan.io/jammy
 sync
 
 echo ""
@@ -43,16 +48,30 @@ echo " setup GRUB and exit without installing grub. The          "
 echo " bootloader is setup later. Picking up incorrect disk      "
 echo " may result in host system getting updated.                "
 echo "==========================================================="
-apt install --assume-yes linux-generic/jammy
+apt install --assume-yes --allow-downgrades linux-generic/jammy
 sync
 
 # Install extra packages
 if [ -d /extrapkgs ]; then
-    apt install --assume-yes /extrapkgs/*.deb
+    apt install --assume-yes --allow-downgrades /extrapkgs/*.deb
 fi
 
 # Disable setserial service as it is changing serial port configuration
 systemctl disable setserial
+
+# Generate host keys 
+ssh-keygen -A
+
+# Generate network configuration
+cat >/etc/netplan/01-netcfg.yaml <<EOF
+# enable dhcp4 and comment out static IP related settings
+network:
+  ethernets:
+    eth0:
+      dhcp4: true
+      dhcp6: false
+  version: 2
+EOF
 
 # Redo GRUB configuration
 cat >/etc/default/grub <<EOF
